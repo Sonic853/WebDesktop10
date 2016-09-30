93fe6a3e39a41a55f259610efcb11acbacc8094e
Sonic853 <sonic853@qq.com>
2016-09-30 23:31:12 +0800

    Re-edit JS, and fix CSS name.

--- js/Desktop10.js
+++ js/Desktop10.js
@@ -1,51 +1,55 @@
-var d = document;
+function D10Id(F){
+return document.getElementById(F);
+}
 window.onload = function(){
-	var z = d.getElementById("Desktop10");
-	var y = d.getElementById("D10-StartButton");
-	var x = d.getElementById("D10-StartMenu");
-	var w = d.getElementById("D10-LeftNav-More");
-	var v = d.getElementById("D10-LeftNav");
-	var u = d.getElementById("D10-LeftNav-Bg");
-	var a = d.getElementById("testeditclass");
+	var z = D10Id("Desktop10");
+	var y = D10Id("D10-StartButton");
+	var x = D10Id("D10-StartMenu");
+	var w = D10Id("D10-LeftNav-More");
+	var v = D10Id("D10-LeftNav");
+	var u = D10Id("D10-LeftNav-Bg");
+	var a = D10Id("testeditclass");
 	a.onclick = function(){
-		if(z.className=="minTaskbar"){
+		if(z.className=="D10-minTaskbar"){
 			z.className = "";
 			a.innerHTML = "Use small Taskbar";
 		}else{
-			z.className = "minTaskbar";
-			a.innerHTML = "Don't Use small Taskbar";
+			z.className = "D10-minTaskbar";
+			a.innerHTML = "Don't use small Taskbar";
 		}
 	}
 	y.onclick = function(){
-		if(x.className=="openStartMenu"){
+		if(x.className=="D10-openStartMenu"){
 			x.className = "";
 			y.className = "";
-		}else{
-			x.className = "openStartMenu";
-			y.className = "StartButtonClicked";
-		}
-	}
-	w.onclick = function(){
-		if(v.className=="openLeftNav"){
 			v.className = "";
 			u.className = "";
 		}else{
-			v.className = "openLeftNav";
-			u.className = "openLeftNav-Bg";
+			x.className = "D10-openStartMenu";
+			y.className = "D10-StartButtonClicked";
+		}
+	}
+	w.onclick = function(){
+		if(v.className=="D10-openLeftNav"){
+			v.className = "";
+			u.className = "";
+		}else{
+			v.className = "D10-openLeftNav";
+			u.className = "D10-openLeftNav-Bg";
 		}
 	}
 	u.onclick = function(){
-		if(v.className=="openLeftNav"){
+		if(v.className=="D10-openLeftNav"){
 			v.className = "";
 			u.className = "";
 		}
 	}
 	z.onmousedown = function(){
-		if(x.className=="openStartMenu"){
+		if(x.className=="D10-openStartMenu"){
 			x.className = "";
 			y.className = "";
 		}
-		if(v.className=="openLeftNav"){
+		if(v.className=="D10-openLeftNav"){
 			v.className = "";
 			u.className = "";
 		}
